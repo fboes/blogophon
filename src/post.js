e9d5971d5e41e06a744db44727e542383d805ebe
Frank Boës <frank.boes@shift.agency>
2016-08-17 08:41:17 +0200

    Whatsapp sharing

--- src/post.js
+++ src/post.js
@@ -8,63 +8,65 @@ var hash            = require('object-hash');
 var toolshed        = require('./js-toolshed/src/js-toolshed');
 var BlogophonUrls   = require('./blogophon-urls')();
 
-var markyMark = function (html, relUrl) {
-  if (relUrl) {
-    html = html.replace(/(!\[.*?\]\()/g, '$1'+relUrl);
-  }
-  var entityMap = {
-    '...': '…',
-    '… …': '…',
-    '(C)': '©',
-    '(R)': '®',
-    '(TM)': '™',
-    '(+-)': '±',
-    '(1/4)': '¼',
-    '(1/2)': '½',
-    '(3/4)': '¾',
-    '->': '→',
-    //'=>': '⇒',
-    '<-': '←',
-    //'<=': '⇐'
-  };
-  html = html.replace(/(\.\.\.|… …|\(C\)|\(R\)|\(TM\)|\(+-\)|\(1\/4\)|\(1\/2\)|\(3\/4\)|->|=>|<-|<=)/g, function (s) {
-    return entityMap[s];
-  });
-  html = html
-    .replace(/\s--\s/g, ' — ')
-    .replace(/(\d)\s*-\s*(\d)/g,'$1–$2')
-    .replace(/(\s)-(\s)/g,'$1–$2')
-    //.replace(/(\d\s*)(x|\*)(\s*\d)/g,'$1×$3')
-    .replace(/([^\S])"(\S.*?\S)"([^\S])/g,'$1„$2“$3')
-    .replace(/([^\S])'(\S.*?\S)'([^\S])/g,'$1‚$2‘$3')
-  ;
-
-  return markdownConvert(html)
-    .replace(/<p>===<\/p>/g,'<!-- more -->')
-    .replace(/(<\/?h)3/g,'$14')
-    .replace(/(<\/?h)2/g,'$13')
-    .replace(/(<\/?h)1/g,'$12')
-    .replace(/(<h2.+?<\/h2>)/,'') // Remove title, will be put into meta.Title
-    .replace(/(<img)/,'$1 itemprop="image"')
-    // <img src="images/articles-1280/article.jpg" srcset="images/articles-640/article.jpg 640w, images/articles-1280/article.jpg 1280w" sizes="100vw" alt="" />
-    .replace(/(href=")([a-zA-Z0-]+)\.md(")/g, '$1' + config.basePath + 'posts/$2/$3')
-    .replace(
-      /<p>\s*(?:<a)?[^>]*?youtube.+v=([a-zA-Z0-9\-_]+)[^>]*?(?:>(.+?)<\/a>)?\s*<\/p>/g,
-      '<div class="video-player youtube"><iframe allowfullscreen="true" src="https://www.youtube.com/embed/$1?enablejsapi=1">$2</iframe></div>'
-    )
-    .replace(
-      /<p>\s*(?:<a)?[^>]*?vimeo.com\/(\d+)[^>]*?(?:>(.+?)<\/a>)?\s*<\/p>/g,
-      '<div class="video-player vimeo"><iframe allowfullscreen="true" src="https://player.vimeo.com/video/$1">$2</iframe></div>'
-    )
-  ;
-  //       '<div class="video-player youtube"><a href="https://www.youtube.com/embed/$2?enablejsapi=1"><img class="preview" src="http://img.youtube.com/vi/$2/sddefault.jpg" alt="" /></a><!--iframe allowfullscreen="true" src="https://www.youtube.com/embed/$2?enablejsapi=1"></iframe--></div>'
-};
-
 /**
  * Represents a single post
  * @constructor
  */
 var Post = function (filename, markdown, meta) {
+  var internal = {
+    markyMark: function (html, relUrl) {
+      if (relUrl) {
+        html = html.replace(/(!\[.*?\]\()/g, '$1'+relUrl);
+      }
+      var entityMap = {
+        '...': '…',
+        '… …': '…',
+        '(C)': '©',
+        '(R)': '®',
+        '(TM)': '™',
+        '(+-)': '±',
+        '(1/4)': '¼',
+        '(1/2)': '½',
+        '(3/4)': '¾',
+        '->': '→',
+        //'=>': '⇒',
+        '<-': '←',
+        //'<=': '⇐'
+      };
+      html = html.replace(/(\.\.\.|… …|\(C\)|\(R\)|\(TM\)|\(+-\)|\(1\/4\)|\(1\/2\)|\(3\/4\)|->|=>|<-|<=)/g, function (s) {
+        return entityMap[s];
+      });
+      html = html
+        .replace(/\s--\s/g, ' — ')
+        .replace(/(\d)\s*-\s*(\d)/g,'$1–$2')
+        .replace(/(\s)-(\s)/g,'$1–$2')
+        //.replace(/(\d\s*)(x|\*)(\s*\d)/g,'$1×$3')
+        .replace(/([^\S])"(\S.*?\S)"([^\S])/g,'$1„$2“$3')
+        .replace(/([^\S])'(\S.*?\S)'([^\S])/g,'$1‚$2‘$3')
+      ;
+
+      return markdownConvert(html)
+        .replace(/<p>===<\/p>/g,'<!-- more -->')
+        .replace(/(<\/?h)3/g,'$14')
+        .replace(/(<\/?h)2/g,'$13')
+        .replace(/(<\/?h)1/g,'$12')
+        .replace(/(<h2.+?<\/h2>)/,'') // Remove title, will be put into meta.Title
+        .replace(/(<img)/,'$1 itemprop="image"')
+        // <img src="images/articles-1280/article.jpg" srcset="images/articles-640/article.jpg 640w, images/articles-1280/article.jpg 1280w" sizes="100vw" alt="" />
+        .replace(/(href=")([a-zA-Z0-]+)\.md(")/g, '$1' + config.basePath + 'posts/$2/$3')
+        .replace(
+          /<p>\s*(?:<a)?[^>]*?youtube.+v=([a-zA-Z0-9\-_]+)[^>]*?(?:>(.+?)<\/a>)?\s*<\/p>/g,
+          '<div class="video-player youtube"><iframe allowfullscreen="true" src="https://www.youtube.com/embed/$1?enablejsapi=1">$2</iframe></div>'
+        )
+        .replace(
+          /<p>\s*(?:<a)?[^>]*?vimeo.com\/(\d+)[^>]*?(?:>(.+?)<\/a>)?\s*<\/p>/g,
+          '<div class="video-player vimeo"><iframe allowfullscreen="true" src="https://player.vimeo.com/video/$1">$2</iframe></div>'
+        )
+      ;
+      //       '<div class="video-player youtube"><a href="https://www.youtube.com/embed/$2?enablejsapi=1"><img class="preview" src="http://img.youtube.com/vi/$2/sddefault.jpg" alt="" /></a><!--iframe allowfullscreen="true" src="https://www.youtube.com/embed/$2?enablejsapi=1"></iframe--></div>'
+    }
+  };
+
   if (!filename) {
     throw new Error('filename is empty');
   }
@@ -89,8 +91,8 @@ var Post = function (filename, markdown, meta) {
   meta.rfcDate     = dateFormat(meta.Date,'ddd, dd mmm yyyy hh:MM:ss o'); // Wed, 02 Oct 2002 15:00:00 +0200
   meta.timestamp   = Math.round(new Date(meta.Date).getTime() / 1000);
 
-  var htmlTeaser   = markyMark(meta.Description.trim(), meta.Url);
-  var html         = markyMark(markdown, meta.Url);
+  var htmlTeaser   = internal.markyMark(meta.Description.trim(), meta.Url);
+  var html         = internal.markyMark(markdown, meta.Url);
 
   if (meta.Title === undefined) {
     meta.Title = markdown.split(/\n/)[0];
@@ -135,6 +137,7 @@ var Post = function (filename, markdown, meta) {
     twitter: "https://twitter.com/intent/tweet?original_referer="+encodeURIComponent(meta.AbsoluteUrl)+"&source=tweetbutton&text="+encodeURIComponent(meta.Title)+"&url="+encodeURIComponent(meta.AbsoluteUrl),
     facebook: "http://www.facebook.com/sharer.php?u="+encodeURIComponent(meta.AbsoluteUrl),
     gplus: "https://plus.google.com/share?url="+encodeURIComponent(meta.AbsoluteUrl),
+    whatsapp: 'whatsapp://send?text=' +encodeURIComponent(meta.Title + ' [' + meta.AbsoluteUrl + ']'),
     mail: "mailto:?subject="+encodeURIComponent('Email von ' + config.name)+"&body="+encodeURIComponent(meta.title+"\n\n"+meta.AbsoluteUrl)+"."
   };
 
