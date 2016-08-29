2a6691293aed42df95a3b1a11855512bf759d03a
Frank Boës <frank.boes@shift.agency>
2016-08-29 08:32:44 +0200

    Hostname from OS as fallback

--- src/config.js
+++ src/config.js
@@ -7,7 +7,9 @@ var pkg            = JSON.parse(fs.readFileSync('./package.json'));
 try {
   config = JSON.parse(fs.readFileSync('./user/config.json'));
 } catch (e) {
+  var os = require("os");
   config = {
+    "baseUrl": "http://" + os.hostname(),
     "basePath": "/",
     "language": "en",
     "itemsPerPage": 5,
@@ -16,8 +18,8 @@ try {
     "deployCmd":"# echo \"Publishing...\" && rsync -az --delete htdocs HOST:PATH && echo \"Published\"",
     "imageSizes": [
       [180,180],
-      [400,225],
-      [800,450]
+      [640,480],
+      [1024,768]
     ]
   };
 }
@@ -26,7 +28,7 @@ config.directories = pkg.directories;
 
 config.directories.currentTheme = pkg.directories.theme + (config.theme ? '/' + config.theme : '');
 
-if (config.deployCmd.match(/^#/)) {
+if (config.deployCmd && config.deployCmd.match(/^#/)) {
   // delete deployment command with `#` at beginning
   config.deployCmd = null;
 }
