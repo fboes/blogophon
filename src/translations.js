f4d52d030c514e758c8249d83cca88989a481a96
Frank Boës <frank.boes@shift.agency>
2016-08-31 08:36:51 +0200

    More translations

--- src/translations.js
+++ src/translations.js
@@ -1,9 +1,14 @@
 'use strict';
 
+/**
+ * Translate strings.
+ * @constructor
+ */
 var Translations = function ( language ) {
+  // Todo: Move languages to separate files
   var translations = {
     en: {
-      'index': 'Homepage',
+      'index': 'Home',
       'page': 'Page %d/%d',
       'tag': 'Articles with tag "%s"'
     },
@@ -12,10 +17,22 @@ var Translations = function ( language ) {
       'page': 'Seite %d/%d',
       'tag': 'Artikel mit dem Tag "%s"'
     },
-    fr: {},
-    es: {},
-    ru: {}
-  }
+    fr: {
+      'index': 'Page d\'accueil',
+      'page': 'Page %d/%d',
+      'tag': 'Articles avec le tag "%s"'
+    },
+    es: {
+      'index': 'Portada',
+      'page': 'Página %d/%d',
+      'tag': 'Artículos con la etiqueta "%s"'
+    },
+    ru: {
+      'index': 'Главная страница',
+      'page': 'Страница %d/%d',
+      'tag': 'Статьи с тегом "%s"'
+    }
+  };
 
   if (!translations[language]) {
     throw new Error("Missing locale "+language);
@@ -24,17 +41,32 @@ var Translations = function ( language ) {
   var currentLanguage = translations[language];
 
   return {
+    /**
+     * [availableLanguageCodes description]
+     * @return {[type]} [description]
+     */
+    availableLanguageCodes: function () {
+      return Object.keys(translations);
+    },
+    /**
+     * [getAll description]
+     * @return {[type]} [description]
+     */
     getAll: function() {
       return currentLanguage;
     },
+    /**
+     * [getString description]
+     * @param  {[type]} key [description]
+     * @return {[type]}     [description]
+     */
     getString: function(key) {
         if (!currentLanguage[key]) {
           throw new Error("Missing string key "+key+" in locale "+language);
         }
         return currentLanguage[key];
     }
-  }
-
+  };
 };
 
 module.exports = Translations;
