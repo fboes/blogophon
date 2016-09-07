b79601a48448b9b27756a55e330054f8e1e8a043
Frank Boës <frank.boes@shift.agency>
2016-09-07 11:01:39 +0200

    Adding test to share links to prevent the mess of last night

--- test/share-links.js
+++ test/share-links.js
@@ -14,3 +14,43 @@ exports.testBasicProperties = function(test) {
 
   test.done();
 };
+
+exports.testBasicProperties = function(test) {
+  'use strict';
+  test.expect(13);
+
+  var testData = {
+    title: 'Gollum',
+    link: 'www.example.com',
+    description: 'Mordor',
+    siteName: 'MiddleEarth',
+  };
+  var share = shareLink(testData.title,testData.link,testData.description,testData.siteName);
+
+  var testMatch = {
+    title      : function(str) { return str.match(new RegExp(testData.title)); },
+    link       : function(str) { return str.match(new RegExp(testData.link)); },
+    description: function(str) { return str.match(new RegExp(testData.description)); },
+    siteName   : function(str) { return str.match(new RegExp(testData.siteName)); },
+  };
+
+  test.ok(share.twitter);
+  test.ok(testMatch.link(share.twitter));
+  test.ok(testMatch.description(share.twitter));
+
+  test.ok(share.facebook);
+  test.ok(testMatch.link(share.facebook));
+
+  test.ok(share.gplus);
+  test.ok(testMatch.link(share.gplus));
+
+  test.ok(share.whatsapp);
+  test.ok(testMatch.link(share.whatsapp));
+  test.ok(testMatch.title(share.whatsapp));
+
+  test.ok(share.email);
+  test.ok(testMatch.link(share.email));
+  test.ok(testMatch.title(share.whatsapp));
+
+  test.done();
+};
