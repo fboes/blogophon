21b6d8c909cffdf9c64bd0ae26308b86e31ee302
Frank Boës <frank.boes@shift.agency>
2016-09-09 16:19:18 +0200

    Documentation

--- src/helpers/url.js
+++ src/helpers/url.js
@@ -6,27 +6,53 @@ var path           = require('path');
 
 /**
  * [Url description]
- * @param {[type]} identifier [description]
+ * @param  {String} identifier [description]
+ * @return {Url}    [description]
  */
 function Url (identifier) {
   this.identifier = identifier;
   return this;
 }
+
+/**
+ * [convert description]
+ * @return {String} [description]
+ */
 Url.prototype.convert = function () {
   return !this.identifier ? null : this.identifier.asciify() + '/index.html';
 };
+
+/**
+ * [relativeUrl description]
+ * @return {String} [description]
+ */
 Url.prototype.relativeUrl = function () {
   var url = this.convert();
   return !url ? null : config.basePath + url.replace(/\/index\.html$/, '/');
 };
+
+/**
+ * [absoluteUrl description]
+ * @return {String} [description]
+ */
 Url.prototype.absoluteUrl = function () {
   var url = this.relativeUrl();
   return !url ? null : config.baseUrl + url;
 };
+
+/**
+ * [filename description]
+ * @return {String} [description]
+ */
 Url.prototype.filename = function () {
   var url = this.relativeUrl();
-  return !url ? null : process.cwd() + '/' + config.directories.htdocs + url.replace(/(\/)$/, '$1index.html');
+  return !url ? null : path.join(process.cwd(), config.directories.htdocs, url.replace(/(\/)$/, '$1index.html'));
 };
+
+/**
+ * [dirname description]
+ * @return {String} [description]
+ */
 Url.prototype.dirname = function () {
   var url = this.filename();
   return !url ? null : path.dirname(url);
