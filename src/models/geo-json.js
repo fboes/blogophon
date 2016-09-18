817e6b74f13bf174c69a8eb68a998089bf128447
Frank Boës <frank.boes@shift.agency>
2016-09-18 08:24:46 +0200

    http://geojson.org/

---
+++ src/models/geo-json.js
@@ -0,0 +1,38 @@
+'use strict';
+
+/**
+ * Returns a GeoJson object. See http://geojson.org/
+ * @constructor
+ */
+var GeoJson = function(index) {
+  return {
+    type: 'FeatureCollection',
+    features: index.map(function(item){
+      var tags = [];
+      if (item.meta.Tags !== undefined) {
+        tags = item.meta.Tags.map(function(t){
+          return t.title;
+        });
+      }
+      return {
+        type: 'Feature',
+        id: item.meta.AbsoluteUrl,
+        properties: {
+          title: item.meta.Title,
+          description: item.safeHtmlTeaser || item.htmlTeaser,
+          link: item.meta.AbsoluteUrl,
+          pubDate: item.meta.Created.rfc,
+          categories: tags
+        },
+        geometry: {
+          type: 'Point',
+          coordinates: [
+            item.meta.Latitude, item.meta.Longitude
+          ]
+        }
+      };
+    })
+  };
+};
+
+module.exports = GeoJson;
---
+++ test/geo-json.js
@@ -0,0 +1,31 @@
+exports.testBasicProperties = function(test) {
+  'use strict';
+  test.expect(8);
+
+  var config = require('../src/config');
+  var item = {
+      htmlTeaser: 1,
+      meta: {
+        AbsoluteUrl: 2,
+        Title: 3,
+        Created: {
+          rfc: 4
+        },
+        tags: [6,7],
+        Longitude: 8,
+        Latitude: 9
+      }
+  };
+  var geojson = require('../src/models/geo-json')([item], new Date(), config);
+
+  test.ok(geojson.type !== undefined);
+  test.ok(geojson.features !== undefined);
+  test.ok(geojson.features[0] !== undefined);
+  test.ok(geojson.features[0].properties !== undefined);
+  test.ok(geojson.features[0].properties.title !== undefined);
+  test.ok(geojson.features[0].geometry !== undefined);
+  test.ok(geojson.features[0].geometry.type !== undefined);
+  test.ok(geojson.features[0].geometry.coordinates !== undefined);
+
+  test.done();
+};
