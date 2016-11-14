d89b0967cf84ea48173573cd91a13576dca53b91
Frank Boës <frank.boes@shift.agency>
2016-11-14 16:12:57 +0100

    More share links

--- src/helpers/share-links.js
+++ src/helpers/share-links.js
@@ -4,24 +4,53 @@
  * Generate object of share links.
  * @param  {String} title       [description]
  * @param  {String} link        [description]
- * @param  {String} description [description]
- * @param  {String} siteName    [description]
+ * @param  {String} description Optional, defaults to `title`
+ * @param  {String} siteName    Optional, defaults to `title`
+ * @param  {String} image       Optional: Absolute image url
  * @return {Object} [description]
  */
-var shareLinks = function(title, link, description, siteName) {
+var shareLinks = function(title, link, description, siteName, image) {
   if (!link) {
-    throw new Error("Missing required URL for share link");
+    throw new Error('Missing required URL for share link');
   }
   title       = title       || '';
   description = description || title;
   siteName    = siteName    || title;
 
+  var urlParameter = function (paramObject) {
+    var param = [];
+    for (var key in paramObject) {
+      param.push(key + '=' + encodeURIComponent(paramObject[key]));
+    }
+    return param.join('&');
+  };
+
   return {
-    twitter: "https://twitter.com/intent/tweet?original_referer="+encodeURIComponent(link)+"&source=tweetbutton&text="+encodeURIComponent(description+' '+link)+"&link="+encodeURIComponent(link),
-    facebook: "http://www.facebook.com/sharer.php?u="+encodeURIComponent(link),
-    gplus: "https://plus.google.com/share?link="+encodeURIComponent(link),
+    facebook: 'http://www.facebook.com/sharer.php?u='+encodeURIComponent(link),
+    gplus:    'https://plus.google.com/share?link='+encodeURIComponent(link),
+    xing:     'https://www.xing.com/spi/shares/new?url='+encodeURIComponent(link),
+    twitter:  'https://twitter.com/intent/tweet?' + urlParameter({
+      original_referer: link,
+      source: 'tweetbutton',
+      text: description+' '+link,
+      link: link
+    }),
+    pinterest:(!image ? null : 'http://pinterest.com/pin/create/button/?'+urlParameter({
+      url: link,
+      media: image,
+      description: description
+    })),
+    linkedin: 'https://www.linkedin.com/shareArticle?'+urlParameter({
+      mini:'true',
+      url:link,
+      title:title,
+      summary:description
+    }),
     whatsapp: 'whatsapp://send?text=' +encodeURIComponent(title + ' [' + link + ']'),
-    email: "mailto:?subject="+encodeURIComponent(siteName)+"&body="+encodeURIComponent(title+"\n\n"+link)
+    email:    'mailto:?'+urlParameter({
+      subject: siteName,
+      body: title+'\n\n'+link
+    })
   };
 };
 
