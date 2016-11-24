9523a0661328e61636f04c5dec7cb8a170860ec4
Frank Boës <frank.boes@shift.agency>
2016-11-24 10:16:27 +0100

    Fixed broken <amp-img>, pt 2

--- test/ampify.js
+++ test/ampify.js
@@ -2,10 +2,13 @@ var ampify = require('../src/helpers/ampify')();
 
 exports.testAmpifyBasic = function testAmpifyBasic(test) {
   'use strict';
-  test.expect(1);
+  test.expect(2);
 
   var html = '<img src="#" alt="" />';
-  test.equal(ampify.ampifyHtml(html), '<amp-img src="#" alt="" layout="responsive"></amp-img>');
+  test.equal(ampify.ampifyHtml(html), '<amp-img layout="responsive" src="#" alt=""></amp-img>');
+
+  html = '<img src="image-320x200.jpg" alt="" />';
+  test.equal(ampify.ampifyHtml(html), '<amp-img layout="responsive" src="image-320x200.jpg" width="320" height="200" alt=""></amp-img>');
 
   test.done();
 };
