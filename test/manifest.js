15b3fea95974a41c675f46c31301e613d7198690
Frank Boës <fboes@3960.org>
2018-03-01 07:14:54 +0100

    Fixing tests for changed themes

--- test/manifest.js
+++ test/manifest.js
@@ -1,7 +1,7 @@
 'use strict';
 
 exports.testBasicProperties = function(test) {
-  test.expect(7);
+  test.expect(6);
 
   const config = require('../lib/config');
   const manifest = require('../lib/models/manifest')(config);
@@ -14,7 +14,6 @@ exports.testBasicProperties = function(test) {
   test.ok(manifest.theme_color !== undefined, 'Theme color is defined');
   test.ok(manifest.start_url !== undefined,   'Start URL is defined');
   test.ok(manifest.icons !== undefined,   'Icons are defined');
-  test.equals(manifest.icons.length, 4,   '4 icons are defined');
 
   test.done();
 };
