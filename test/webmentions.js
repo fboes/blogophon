9170448ccd1b979eded8acb4738d750dea080661
Frank Boës <fboes@3960.org>
2018-07-20 19:27:28 +0200

    Solving almost all test cases of https://webmention.rocks/

--- test/webmentions.js
+++ test/webmentions.js
@@ -56,33 +56,33 @@ describe('Webmentions', function() {
   });
 
   const discoverUrls = [
-    // 'https://webmention.rocks/test/1',
-    // 'https://webmention.rocks/test/2',
-    // 'https://webmention.rocks/test/3',
-    // 'https://webmention.rocks/test/4',
-    // 'https://webmention.rocks/test/5',
-    // 'https://webmention.rocks/test/6',
-    // 'https://webmention.rocks/test/7',
-    // 'https://webmention.rocks/test/8',
-    // 'https://webmention.rocks/test/9',
-    // 'https://webmention.rocks/test/10',
-    // 'https://webmention.rocks/test/11',
-    // 'https://webmention.rocks/test/12',
-    // 'https://webmention.rocks/test/13',
-    // 'https://webmention.rocks/test/14',
-    // 'https://webmention.rocks/test/15',
-    // 'https://webmention.rocks/test/16',
-    // 'https://webmention.rocks/test/17',
-    // 'https://webmention.rocks/test/18',
-    // 'https://webmention.rocks/test/19',
-    // 'https://webmention.rocks/test/20',
-    // 'https://webmention.rocks/test/21',
-    // 'https://webmention.rocks/test/22',
-    // 'https://webmention.rocks/test/23/page'
+    'https://webmention.rocks/test/1',
+    'https://webmention.rocks/test/2',
+    'https://webmention.rocks/test/3',
+    'https://webmention.rocks/test/4',
+    'https://webmention.rocks/test/5',
+    'https://webmention.rocks/test/6',
+    'https://webmention.rocks/test/7',
+    'https://webmention.rocks/test/8',
+    'https://webmention.rocks/test/9',
+    'https://webmention.rocks/test/10',
+    'https://webmention.rocks/test/11',
+    'https://webmention.rocks/test/12',
+    'https://webmention.rocks/test/13',
+    'https://webmention.rocks/test/14',
+    'https://webmention.rocks/test/15',
+    'https://webmention.rocks/test/16',
+    'https://webmention.rocks/test/17',
+    'https://webmention.rocks/test/18',
+    'https://webmention.rocks/test/19',
+    'https://webmention.rocks/test/20',
+    'https://webmention.rocks/test/21',
+    'https://webmention.rocks/test/22',
+    'https://webmention.rocks/test/23/page'
   ];
   discoverUrls.forEach(function(url) {
-    it('should discover endpoint URLs on test URL ' + url, function() {
-      assert.ok(webmentions.sendMentions(url, [url]));
+    it.skip('should discover endpoint URLs on test URL ' + url, function() {
+      assert.ok(webmentions.sendMentions('https://3960.org/sandbox/webmention.html', [url]));
     });
   });
 });
