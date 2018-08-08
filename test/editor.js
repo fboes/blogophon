ebd33a50ee55e33addaa6d4df342ab2aed1e053f
Frank Boës <fboes@3960.org>
2018-08-08 19:51:38 +0200

    Add delete method

--- test/editor.js
+++ test/editor.js
@@ -7,20 +7,33 @@ describe('Ampify', function() {
   const config = require('../lib/config')(__dirname);
   const editor = require('../lib/editor')(config);
 
-  it('should convert titleForFilename', function() {
+  it('should do titleForFilename', function() {
     assert.equal(editor.titleForFilename('Foo and Bar', { postFileMode: null }, '2018-08-07'), 'Foo and Bar');
     assert.equal(editor.titleForFilename('Foo and Bar', { postFileMode: '' }, '2018-08-07'), 'Foo and Bar');
     assert.equal(editor.titleForFilename('Foo and Bar', { postFileMode: 'Title' }, '2018-08-07'), 'Foo and Bar');
     assert.equal(editor.titleForFilename('Foo and Bar', { postFileMode: 'Title-Date' }, '2018-08-07'), 'Foo and Bar-2018-08-07');
   });
 
-  it('should convert filenameFromTitle', function() {
+  it('should do filenameFromTitle', function() {
     assert.equal(editor.filenameFromTitle('Foo and Bar'), path.join(config.directories.data, 'foo-bar', 'index.md'));
   });
 
-  it('should convert dirnameFromFilename', function() {
-    assert.equal(editor.dirnameFromFilename('/bla/index.md'), path.dirname('/bla/index.md'));
-    assert.equal(editor.dirnameFromFilename('/bla/index.md'), '/bla');
+  it('should do dirnameFromFilename', function() {
+    assert.equal(editor.dirnameFromFilename('/post/bla/index.md'), path.dirname('/post/bla/index.md'));
+    assert.equal(editor.dirnameFromFilename('/post/bla/index.md'), '/post/bla');
   });
 
+  it('should do isSlugInDirectory', function() {
+    assert.equal(editor.isSlugInDirectory('/bla/index.md'), true);
+    assert.equal(editor.isSlugInDirectory('/post/bla/index.md'), true);
+    assert.equal(editor.isSlugInDirectory('/bla/blubb.md'), false);
+    assert.equal(editor.isSlugInDirectory('/post/bla/blubb.md'), false);
+  });
+
+  it('should do getSlugFromFilename', function() {
+    assert.equal(editor.getSlugFromFilename('/bla/index.md'), 'bla');
+    assert.equal(editor.getSlugFromFilename('/post/bla/index.md'), 'bla');
+    assert.equal(editor.getSlugFromFilename('/bla/blubb.md'), 'blubb');
+    assert.equal(editor.getSlugFromFilename('/post/bla/blubb.md'), 'blubb');
+  });
 });
